fcc7f9db6c2e4eee03b1c72eda0f3fe94e872456
lmariel <laurenmariel90@gmail.com>
2018-07-11 16:22:26 -0400

    Demo of adding component and styling via a stylesheet

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,4 +2,9 @@ import Typography from 'typography';
 
 const typography = new Typography({ baseFontSize: '18px' });
 
+// TODO: Possibly add a theme
+// https://www.gatsbyjs.org/tutorial/part-two/
+// http://kyleamathews.github.io/typography.js
+// https://github.com/KyleAMathews/typography.js#published-typographyjs-themes
+
 export default typography;
